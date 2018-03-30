7c51b7faf22dd4d618b2145fb818f7fa8585659c
Brian Wilson <brian@wildsong.biz>
2018-03-30 10:35:33 -0700

    rearranged project

--- Toolbox/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-#    Currently this file is just here so we can load the files in this folder
-#    as modules from up above...  it's probably good for other things too. RTFM
-#
